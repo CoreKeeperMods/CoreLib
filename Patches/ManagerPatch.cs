c345cdd061fdbfd93b053b42010d63dab0301a3f
JrProgrammer <mail@jessevermeulen.nl>
2022-03-24 22:27:01 +0100

    Added IsInGame query to see if the game has started

--- Patches/ManagerPatch.cs
+++ Patches/ManagerPatch.cs
@@ -8,7 +8,7 @@ namespace CoreLib.Patches {
 
         [HarmonyPatch("Awake")]
         [HarmonyPostfix]
-        static void AwakePatch(Manager __instance) {
+        static void AwakePostfix(Manager __instance) {
             CoreLib.Manager = __instance;
             CoreLib.Logger.LogInfo("Patched Manager");
         }
---
+++ State/State.cs
@@ -0,0 +1,8 @@
+﻿namespace CoreLib {
+    public class State {
+        public static bool IsInGame() {
+            Manager manager = GameManagers.GetMainManager();
+            return manager.currentSceneHandler.isInGame;
+        }
+    }
+}
