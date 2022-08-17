a17462d0b9ed4aec6cdb84c9bd47a57161f3317f
kremnev8 <kremnev8super@yandex.ru>
2022-08-17 20:30:59 +0300

    Add drop table modification feature

---
+++ CoreLib/Submodules/DropTables/Patches/LootTableBank_Patch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using HarmonyLib;
+using List = Il2CppSystem.Collections.Generic.List<LootInfo>;
+
+namespace CoreLib.Submodules.DropTables.Patches;
+
+public static class LootTableBank_Patch
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(LootTableBank), "InitLoot")]
+    public static void GetLootTableBank(LootTable lootTable, List lootInfos, int minUniqueDrops, int maxUniqueDrops, List guaranteedLootInfos)
+    {
+        try
+        {
+            if (DropTablesModule.dropTableModification.ContainsKey(lootTable.id))
+            {
+                DropTableModificationData modificationData = DropTablesModule.dropTableModification[lootTable.id];
+                if (lootInfos != null && guaranteedLootInfos != null)
+                {
+                    DropTablesModule.RemoveDrops(lootInfos, guaranteedLootInfos, modificationData);
+                    DropTablesModule.EditDrops(lootTable, lootInfos, guaranteedLootInfos, modificationData);
+                    DropTablesModule.AddDrops(lootTable, lootInfos, guaranteedLootInfos, modificationData);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            CoreLibPlugin.Logger.LogWarning($"Failed to update loot tables:\n{e}");
+        }
+    }
+}
