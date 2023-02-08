082287a7341a30ce7a1e5212e0d10b826013d2a9
kremnev8 <kremnev8super@yandex.ru>
2023-02-08 16:18:26 +0300

    Fix EntityMonoBehavior scripts

--- CoreLib/Components/ModProjectile.cs
+++ CoreLib/Components/ModProjectile.cs
@@ -5,30 +5,14 @@ namespace CoreLib.Components
 {
     public class ModProjectile : Projectile, IAllocate
     { 
-        private GCHandle optionalColorReplacersHandle;
-        private GCHandle optionalColorSpriteSheetSkinsHandle;
-        private GCHandle colorSpriteSheetsHandle;
-        private GCHandle optionalColorSpriteRenderersHandle;
-        private GCHandle defaultColorSpritesHandle;
-        private GCHandle colorSpritesPerSpriteRendererHandle;
-        private GCHandle optionalSpriteRenderersToTintHandle;
-        private GCHandle colorTintsHandle;
-        private GCHandle defaultColorTintsHandle;
-        private GCHandle optionalLightsToPaintHandle;
-        private GCHandle optionalLightsDefaultColorsHandle;
-        private GCHandle lightPaintColorsHandle;
-        private GCHandle optionalSpriteSheetSkinsHandle;
-        private GCHandle skinSpriteSheetsHandle;
-        private GCHandle seasonalSkinSpriteSheetsHandle;
-        private GCHandle spritesToRandomlyOffsetSlightlyOnZHandle;
-        private GCHandle sfxParamsHandle;
-        private GCHandle puffParamsHandle;
+        private GCHandle reskinOptionsHandle;
+        private GCHandle paintableOptionssHandle;
+        private GCHandle soundOptionsHandle;
+        private GCHandle particleOptionsHandle;
         private GCHandle objectVariationsHandle;
-        private GCHandle particleSpawnLocationsHandle;
         private GCHandle spritesToRandomlyFlipHandle;
-        private GCHandle particlesToDisableOnLowQualityHandle;
         private GCHandle conditionsEffectsSettingsHandle;
-        private GCHandle defaultSpritePositionsHandle;
+        
         private GCHandle conditionsHandle;
         private GCHandle piercesWallTypesHandle;
         private GCHandle tilesToCheckHandle;
@@ -47,30 +31,14 @@ namespace CoreLib.Components
         {
             if (allocated) return false;
 
-            optionalColorReplacersHandle = GCHandle.Alloc(optionalColorReplacers);
-            optionalColorSpriteSheetSkinsHandle = GCHandle.Alloc(optionalColorSpriteSheetSkins);
-            colorSpriteSheetsHandle = GCHandle.Alloc(colorSpriteSheets);
-            optionalColorSpriteRenderersHandle = GCHandle.Alloc(optionalColorSpriteRenderers);
-            defaultColorSpritesHandle = GCHandle.Alloc(defaultColorSprites);
-            colorSpritesPerSpriteRendererHandle = GCHandle.Alloc(colorSpritesPerSpriteRenderer);
-            optionalSpriteRenderersToTintHandle = GCHandle.Alloc(optionalSpriteRenderersToTint);
-            colorTintsHandle = GCHandle.Alloc(colorTints);
-            defaultColorTintsHandle = GCHandle.Alloc(defaultColorTints);
-            optionalLightsToPaintHandle = GCHandle.Alloc(optionalLightsToPaint);
-            optionalLightsDefaultColorsHandle = GCHandle.Alloc(optionalLightsDefaultColors);
-            lightPaintColorsHandle = GCHandle.Alloc(lightPaintColors);
-            optionalSpriteSheetSkinsHandle = GCHandle.Alloc(optionalSpriteSheetSkins);
-            skinSpriteSheetsHandle = GCHandle.Alloc(skinSpriteSheets);
-            seasonalSkinSpriteSheetsHandle = GCHandle.Alloc(seasonalSkinSpriteSheets);
-            spritesToRandomlyOffsetSlightlyOnZHandle = GCHandle.Alloc(spritesToRandomlyOffsetSlightlyOnZ);
-            sfxParamsHandle = GCHandle.Alloc(sfxParams);
-            puffParamsHandle = GCHandle.Alloc(puffParams);
+            reskinOptionsHandle = GCHandle.Alloc(reskinOptions);
+            paintableOptionssHandle = GCHandle.Alloc(paintableOptions);
+            soundOptionsHandle = GCHandle.Alloc(soundOptions);
+            particleOptionsHandle = GCHandle.Alloc(particleOptions);
             objectVariationsHandle = GCHandle.Alloc(objectVariations);
-            particleSpawnLocationsHandle = GCHandle.Alloc(particleSpawnLocations);
             spritesToRandomlyFlipHandle = GCHandle.Alloc(spritesToRandomlyFlip);
-            particlesToDisableOnLowQualityHandle = GCHandle.Alloc(particlesToDisableOnLowQuality);
             conditionsEffectsSettingsHandle = GCHandle.Alloc(conditionsEffectsSettings);
-            defaultSpritePositionsHandle = GCHandle.Alloc(defaultSpritePositions);
+            
             conditionsHandle = GCHandle.Alloc(conditions);
             piercesWallTypesHandle = GCHandle.Alloc(piercesWallTypesHandle);
             tilesToCheckHandle = GCHandle.Alloc(tilesToCheck);
@@ -83,30 +51,14 @@ namespace CoreLib.Components
         {
             this.CallBase<PoolableSimple>(nameof(OnDestroy));
 
-            optionalColorReplacersHandle.Free();
-            optionalColorSpriteSheetSkinsHandle.Free();
-            colorSpriteSheetsHandle.Free();
-            optionalColorSpriteRenderersHandle.Free();
-            defaultColorSpritesHandle.Free();
-            colorSpritesPerSpriteRendererHandle.Free();
-            optionalSpriteRenderersToTintHandle.Free();
-            colorTintsHandle.Free();
-            defaultColorTintsHandle.Free();
-            optionalLightsToPaintHandle.Free();
-            optionalLightsDefaultColorsHandle.Free();
-            lightPaintColorsHandle.Free();
-            optionalSpriteSheetSkinsHandle.Free();
-            skinSpriteSheetsHandle.Free();
-            seasonalSkinSpriteSheetsHandle.Free();
-            spritesToRandomlyOffsetSlightlyOnZHandle.Free();
-            sfxParamsHandle.Free();
-            puffParamsHandle.Free();
+            reskinOptionsHandle.Free();
+            paintableOptionssHandle.Free();
+            soundOptionsHandle.Free();
+            particleOptionsHandle.Free();
             objectVariationsHandle.Free();
-            particleSpawnLocationsHandle.Free();
             spritesToRandomlyFlipHandle.Free();
-            particlesToDisableOnLowQualityHandle.Free();
             conditionsEffectsSettingsHandle.Free();
-            defaultSpritePositionsHandle.Free();
+            
             conditionsHandle.Free();
             piercesWallTypesHandle.Free();
             tilesToCheckHandle.Free();
