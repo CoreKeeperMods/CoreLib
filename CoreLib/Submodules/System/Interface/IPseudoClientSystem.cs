74d33ef119a576e58cb92a0d357630c8b9237f05
kremnev8 <kremnev8super@yandex.ru>
2023-03-03 22:26:02 +0300

    DOCS: Add documentation

--- CoreLib/Submodules/System/Interface/IPseudoClientSystem.cs
+++ CoreLib/Submodules/System/Interface/IPseudoClientSystem.cs
@@ -2,9 +2,20 @@
 
 namespace CoreLib.Submodules.ModSystem
 {
+    /// <summary>
+    /// Define Pseudo Client System by implementing this interface
+    /// Pseudo Systems can perform actions on certain entities every tick 
+    /// </summary>
     public interface IPseudoClientSystem
     {
+        /// <summary>
+        /// Execute after client world is started
+        /// </summary>
         void OnClientStarted(World world);
+        
+        /// <summary>
+        /// Client world is about to be destroyed. Clear the reference to it
+        /// </summary>
         void OnClientStopped();
     }
 }
