209aab76c42b2d1afa8f47e9dfbcc5295667efc8
kremnev8 <kremnev8super@yandex.ru>
2023-03-06 13:46:40 +0300

    Bump Version

--- CoreLib/CoreLibPlugin.cs
+++ CoreLib/CoreLibPlugin.cs
@@ -18,7 +18,7 @@ public class CoreLibPlugin : BasePlugin
     public const string NAME = "CoreLib";
     public const string VERSION = ThisAssembly.AssemblyVersion;
         
-    public static readonly GameVersion buildFor = new GameVersion(0,5,2, 2, "a842");
+    public static readonly GameVersion buildFor = new GameVersion(0,5,2, 5, "47b7");
     internal static HashSet<string> LoadedSubmodules;
     internal static APISubmoduleHandler submoduleHandler;
     internal static Harmony harmony;
