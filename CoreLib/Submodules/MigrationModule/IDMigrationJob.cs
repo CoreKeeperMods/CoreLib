0668ceeb39251c8ea6a8d28e23ec827b10afe7d1
kremnev8 <kremnev8super@yandex.ru>
2023-06-05 18:28:47 +0300

    Use Job for migration module

---
+++ CoreLib/Submodules/MigrationModule/IDMigrationJob.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using CoreLib.Submodules.ModComponent;
+using CoreLib.Submodules.ModSystem;
+using CoreLib.Submodules.ModSystem.Jobs;
+using Unity.Collections;
+using Unity.Entities;
+using UnityEngine;
+
+namespace CoreLib.Submodules.MigrationModule
+{
+    public struct IDMigrationJob : JobExtensions.IModJob
+    {
+        public NativeArray<Entity> objectsEntities;
+        public NativeArray<Entity> inventoryEntities;
+        public ModComponentDataFromEntity<ObjectDataCD> objectDataFromEntity;
+        public ModBufferFromEntity<ContainedObjectsBuffer> containedObjectsFromEntity;
+
+        public void Execute()
+        {
+            EntityCommandBuffer ecb = new EntityCommandBuffer(Allocator.Temp);
+
+            EntityPass(ecb);
+            InventoryPass(ecb);
+
+            ecb.Playback(Manager.ecs.ServerWorld.EntityManager);
+            ecb.Dispose();
+        }
+        
+        private void EntityPass(EntityCommandBuffer ecb)
+        {
+            foreach (Entity entity in objectsEntities)
+            {
+                ObjectDataCD objectDataCd = objectDataFromEntity[entity];
+                if (!PugDatabase.HasObject(objectDataCd.objectID))
+                {
+                    CoreLibPlugin.Logger.LogInfo($"Found missing object with ID: {objectDataCd.objectID}, removing!");
+                    ecb.DestroyEntity(entity);
+                }
+            }
+
+            objectsEntities.Dispose();
+        }
+        
+        private void InventoryPass(EntityCommandBuffer ecb)
+        {
+            foreach (Entity entity in inventoryEntities)
+            {
+                ModDynamicBuffer<ContainedObjectsBuffer> objectsBuffer = containedObjectsFromEntity[entity];
+
+                for (int i = 0; i < objectsBuffer.Count; i++)
+                {
+                    ContainedObjectsBuffer item = objectsBuffer[i];
+                    if (item.objectID == ObjectID.None) continue;
+                    
+                    if (!PugDatabase.HasObject(item.objectID))
+                    {
+                        CoreLibPlugin.Logger.LogInfo($"Found missing object with ID: {item.objectID} in inventory, removing!");
+           
+                        item.objectData.objectID = ObjectID.None;
+                        item.objectData.amount = 0;
+                        item.objectData.variation = 0;
+                        item.auxDataIndex = 0;
+                        objectsBuffer[i] = item;
+                    }
+                }
+            }
+
+            inventoryEntities.Dispose();
+        }
+        
+    }
+}
--- CoreLib/Submodules/MigrationModule/MigrationModule.cs
+++ CoreLib/Submodules/MigrationModule/MigrationModule.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Reflection;
+using CoreLib.Submodules.ModComponent;
 using CoreLib.Submodules.ModEntity;
 using CoreLib.Submodules.ModSystem;
+using CoreLib.Submodules.ModSystem.Jobs;
 using Il2CppInterop.Runtime.Injection;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
 
 namespace CoreLib.Submodules.MigrationModule
 {
@@ -44,8 +49,35 @@ namespace CoreLib.Submodules.MigrationModule
         [CoreLibSubmoduleInit(Stage = InitStage.PostLoad)]
         internal static void Load()
         {
-            ClassInjector.RegisterTypeInIl2Cpp<IDMigrationSystem>();
-            SystemModule.RegisterSystem<IDMigrationSystem>();
+            ClassInjector.RegisterTypeInIl2Cpp<IDMigrationJob>();
+            SystemModule.OnServerWorldStarted += ScheduleMigration;
+        }
+
+        internal static void ScheduleMigration(World world)
+        {
+            var objectsQuery = world.EntityManager.CreateEntityQuery(new EntityQueryDesc()
+            {
+                All = new[] { ComponentModule.ReadOnly<ObjectDataCD>() },
+                Options = EntityQueryOptions.IncludeDisabled
+            });
+            
+            var inventoriesQuery = world.EntityManager.CreateEntityQuery(new EntityQueryDesc()
+            {
+                All = new[] { ComponentModule.ReadOnly<InventoryCD>() },
+                Options = EntityQueryOptions.IncludeDisabled
+            });
+
+            IDMigrationJob migrationJob = new IDMigrationJob()
+            {
+                objectsEntities = objectsQuery.ToEntityArrayAsync(Allocator.TempJob, out JobHandle handle1),
+                inventoryEntities = inventoriesQuery.ToEntityArrayAsync(Allocator.TempJob, out JobHandle handle2),
+                objectDataFromEntity = new ModComponentDataFromEntity<ObjectDataCD>(world.EntityManager),
+                containedObjectsFromEntity = new ModBufferFromEntity<ContainedObjectsBuffer>(world.EntityManager, false)
+            };
+            
+            JobHandle jobsHandle = JobHandle.CombineDependencies(handle1, handle2);
+            JobHandle myJobHandle = migrationJob.ModSchedule(jobsHandle);
+            myJobHandle.Complete();
         }
 
         #endregion
