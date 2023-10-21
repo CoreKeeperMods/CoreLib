51088d729f345147130310c665f5b7c8fb0fb248
kremnev8 <kremnev8super@yandex.ru>
2023-10-21 18:52:02 +0300

    Add ModSDK files

---
+++ Assets/ModSDK/SDK/Editor/ModBuilderSettings.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using PugMod;
+using UnityEditor;
+using UnityEngine;
+
+public class ModBuilderSettings : ScriptableObject
+{
+	public ModMetadata metadata = new ModMetadata
+	{
+		guid = Guid.NewGuid().ToString("N"),
+		name = "MyMod",
+	};
+	
+	public string modPath = "Assets/Mod";
+
+	public bool forceReimport = true;
+	public bool buildBundles = true;
+	public bool buildLinux = false;
+	
+	public bool buildBurst = true;
+
+	private void OnValidate()
+	{
+		if (string.IsNullOrEmpty(modPath))
+		{
+			var path = AssetDatabase.GetAssetPath(this);
+			modPath = Path.GetDirectoryName(path);
+		}
+	}
+}
