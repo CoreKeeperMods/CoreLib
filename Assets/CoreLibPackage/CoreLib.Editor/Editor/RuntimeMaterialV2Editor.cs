aafb0397f19ddf7cf7dd1c9e887dfb5c5f7fdd29
limoka <kremnev8super@yandex.ru>
2024-06-21 21:39:26 +0300

    Update Editor script

--- Assets/CoreLibPackage/CoreLib.Editor/Editor/RuntimeMaterialV2Editor.cs
+++ Assets/CoreLibPackage/CoreLib.Editor/Editor/RuntimeMaterialV2Editor.cs
@@ -13,46 +13,15 @@ namespace CoreLib.Editor
             RuntimeMaterial runtimeMaterial = (RuntimeMaterial)target;
 
             base.OnInspectorGUI();
-            
+
             if (GUILayout.Button("Use material name"))
             {
-                
-                SpriteRenderer renderer = runtimeMaterial.GetComponent<SpriteRenderer>();
-                if (renderer != null)
-                {
-                    runtimeMaterial.materialName = renderer.sharedMaterial.name;
-                }
-            
-                ParticleSystemRenderer particleSystemRenderer = runtimeMaterial.GetComponent<ParticleSystemRenderer>();
-                if (particleSystemRenderer != null)
-                {
-                    runtimeMaterial.materialName = particleSystemRenderer.sharedMaterial.name;
-                }
-                EditorUtility.SetDirty(runtimeMaterial);
+                runtimeMaterial.UseMaterialName();
             }
             
-            if (GUILayout.Button("Reassign material")){
-                SpriteRenderer renderer = runtimeMaterial.GetComponent<SpriteRenderer>();
-                ParticleSystemRenderer particleSystemRenderer = runtimeMaterial.GetComponent<ParticleSystemRenderer>();
-
-            
-
-                string[] results = AssetDatabase.FindAssets($"t:material {runtimeMaterial.materialName}");
-                if (results.Length > 0)
-                {
-                    string result = results.First();
-                    string path = AssetDatabase.GUIDToAssetPath(result);
-                    if (renderer != null)
-                        renderer.sharedMaterial = AssetDatabase.LoadAssetAtPath<Material>(path);
-                    if (particleSystemRenderer != null)
-                        particleSystemRenderer.sharedMaterial = AssetDatabase.LoadAssetAtPath<Material>(path);
-
-                    EditorUtility.SetDirty(runtimeMaterial);
-                }
-                else
-                {
-                    Debug.Log("No matches found!");
-                }
+            if (GUILayout.Button("Reassign material"))
+            {
+                runtimeMaterial.ReassignMaterial();
             }
         }
     }
