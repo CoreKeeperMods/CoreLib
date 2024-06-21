60670823eebbe0cfdb742e39872a77f3166504b3
limoka <kremnev8super@yandex.ru>
2024-06-21 21:33:26 +0300

    Expand Debug class functionality to allow displaying lines

--- Assets/CoreLibPackage/CoreLib/Scripts/Util/Debug.cs
+++ Assets/CoreLibPackage/CoreLib/Scripts/Util/Debug.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using CoreLib.Util.Extensions;
 using Unity.Mathematics;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace CoreLib.Util
 {
@@ -10,32 +12,64 @@ namespace CoreLib.Util
     /// </summary>
     public static class Debug
     {
+        [Serializable]
         public struct MarkerData
         {
-            public GameObject gameObject;
-            public MeshRenderer renderer;
+            public bool isLine;
 
-            public MarkerData(GameObject gameObject, MeshRenderer renderer)
+            public GameObject gameObject;
+            public MeshRenderer meshRenderer;
+            public MeshRenderer quadRenderer;
+            //public LineRenderer lineRenderer;
+
+            public MarkerData(GameObject gameObject, MeshRenderer meshRenderer, MeshRenderer quadRenderer)
             {
                 this.gameObject = gameObject;
-                this.renderer = renderer;
+                this.meshRenderer = meshRenderer;
+                this.quadRenderer = quadRenderer;
+
+                isLine = this.quadRenderer != null;
             }
 
             public void Set(Vector3 pos, Color color)
             {
-                renderer.material.color = color;
+                if (isLine) return;
+                
+                meshRenderer.material.color = color;
                 gameObject.transform.localPosition = pos;
                 gameObject.SetActive(true);
             }
+
+            public void SetLine(Vector3 pos1, Vector3 pos2, Color color)
+            {
+                if (!isLine) return;
+                
+                quadRenderer.material.color = color;
+
+                var dist = Vector3.Distance(pos1, pos2);
+                var mid = (pos1 + pos2) / 2;
+                var dir = (pos2 - pos1).normalized;
+                var rotation = Vector2.SignedAngle(Vector2.right, new Vector2(dir.x, dir.z));
+                
+                gameObject.transform.localPosition = mid;
+                gameObject.transform.localScale = new Vector3(dist, 0.1f, 1);
+                gameObject.transform.localRotation = Quaternion.Euler(-90, -rotation, 0);
+                
+                //lineRenderer.SetPosition(0, pos1);
+                //lineRenderer.SetPosition(1, pos2);
+                gameObject.SetActive(true);
+            } 
 
             public void Clear()
             {
                 gameObject.SetActive(false);
             }
         }
-        
+
         private static List<MarkerData> activeMarkers = new List<MarkerData>();
-        private static Queue<MarkerData> freeMarkers = new Queue<MarkerData>();
+        
+        private static Queue<MarkerData> freePointMarkers = new Queue<MarkerData>();
+        private static Queue<MarkerData> freeLineMarkers = new Queue<MarkerData>();
 
         /// <summary>
         /// Place a colored dot at specified position
@@ -44,7 +78,7 @@ namespace CoreLib.Util
         {
             DisplayPos(pos.ToVector3(), color);
         }
-        
+
         /// <summary>
         /// Place a colored dot at specified position
         /// </summary>
@@ -70,7 +104,7 @@ namespace CoreLib.Util
         {
             DisplayLocalPos(pos.ToVector3(), color);
         }
-        
+
         /// <summary>
         /// Place a colored dot at specified position
         /// </summary>
@@ -78,14 +112,40 @@ namespace CoreLib.Util
         {
             DisplayLocalPos(pos.ToVector3(), color);
         }
-        
+
         /// <summary>
         /// Place a colored dot at specified position
         /// </summary>
         public static void DisplayLocalPos(Vector3 pos, Color color)
         {
-            MarkerData marker = CreateMarker();
+            MarkerData marker = CreateMarker(false);
+            pos.y += 1f;
+            pos.z -= 1f;
             marker.Set(pos, color);
+
+            activeMarkers.Add(marker);
+        }
+
+        public static void DisplayLine(float3 pos1, float3 pos2, Color color)
+        {
+            DisplayLine(pos1.ToVector3(), pos2.ToVector3(), color);
+        }
+        
+        public static void DisplayLine(int3 pos1, int3 pos2, Color color)
+        {
+            DisplayLine(pos1.ToVector3(), pos2.ToVector3(), color);
+        }
+
+        public static void DisplayLine(Vector3 pos1, Vector3 pos2, Color color)
+        {
+            MarkerData marker = CreateMarker(true);
+
+            pos1.y += 1f;
+            pos1.z -= 1f;
+            
+            pos2.y += 1f;
+            pos2.z -= 1f;
+            marker.SetLine(pos1, pos2, color);
 
             activeMarkers.Add(marker);
         }
@@ -98,29 +158,79 @@ namespace CoreLib.Util
             foreach (MarkerData marker in activeMarkers)
             {
                 marker.Clear();
-                freeMarkers.Enqueue(marker);
+
+                if (marker.isLine)
+                {
+                    freeLineMarkers.Enqueue(marker);
+                }
+                else
+                {
+                    freePointMarkers.Enqueue(marker);
+                }
             }
 
             activeMarkers.Clear();
         }
-        
-        private static MarkerData CreateMarker()
+
+        private static MarkerData CreateMarker(bool line)
         {
-            if (freeMarkers.Count > 0)
+            if (!line)
             {
-                return freeMarkers.Dequeue();
+                if (freePointMarkers.Count > 0)
+                    return freePointMarkers.Dequeue();
+                
+                return CreatePointMarker();
             }
-            
+            else
+            {
+                if (freeLineMarkers.Count > 0)
+                    return freeLineMarkers.Dequeue();
+                
+                return CreateLineMarker();
+            }
+        }
+
+        private static MarkerData CreatePointMarker()
+        {
             GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            
+            sphere.name = "DebugPoint";
+
             MeshRenderer meshRenderer = sphere.GetComponent<MeshRenderer>();
             Object.Destroy(sphere.GetComponent<SphereCollider>());
             meshRenderer.material.shader = Shader.Find("Radical/SpritesDefault");
             sphere.transform.parent = Manager.camera.OrigoTransform;
             sphere.transform.localScale *= 0.2f;
 
-            MarkerData data = new MarkerData(sphere, meshRenderer);
+            MarkerData data = new MarkerData(sphere, meshRenderer, null);
             return data;
         }
+
+        private static MarkerData CreateLineMarker()
+        {
+            //GameObject line = new GameObject("DebugLine");
+            
+            GameObject line = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            line.name = "DebugLine";
+            
+            /*LineRenderer lineRenderer = line.AddComponent<LineRenderer>();
+            
+            var material = new Material(Shader.Find("Radical/SpritesDefault"));
+            lineRenderer.material = material;
+            lineRenderer.startWidth = 0.1f;
+            lineRenderer.endWidth = 0.1f;
+            lineRenderer.positionCount = 2;
+            lineRenderer.useWorldSpace = false;*/
+            
+            MeshRenderer meshRenderer = line.GetComponent<MeshRenderer>();
+            Object.Destroy(line.GetComponent<MeshCollider>());
+            meshRenderer.material.shader = Shader.Find("Radical/SpritesDefault");
+
+            line.transform.parent = Manager.camera.OrigoTransform;
+            //line.transform.localScale *= 0.2f;
+
+            MarkerData data = new MarkerData(line, null, meshRenderer);
+            return data;
+        }
+        
     }
 }
