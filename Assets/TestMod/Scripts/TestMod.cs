372df2606b2bb748b409695aee7e55ca10cf943e
limoka8 <liza.kremneva.8@yandex.ru>
2026-01-13 00:07:22 +0300

    Add test mod

---
+++ Assets/TestMod/Scripts/TestMod.cs
@@ -0,0 +1,51 @@
+﻿using CoreLib;
+using CoreLib.Submodule.Audio;
+using CoreLib.Submodule.Command;
+using CoreLib.Submodule.ControlMapping;
+using CoreLib.Submodule.Entity;
+using CoreLib.Submodule.EquipmentSlot;
+using CoreLib.Submodule.LootDrop;
+using CoreLib.Submodule.TileSet;
+using CoreLib.Submodule.UserInterface;
+using PugMod;
+using UnityEngine;
+using Logger = CoreLib.Util.Logger;
+
+namespace TestMod.Scripts
+{
+    public class TestMod : IMod
+    {
+        internal static Logger Log = new Logger("TestMod");
+        
+        public void EarlyInit()
+        {
+            Log.LogInfo($"Loading CoreLib Test Mod");
+            CoreLibMod.LoadSubmodule(
+                typeof(AudioModule),
+                typeof(CommandModule),
+                typeof(ControlMappingModule),
+                typeof(EntityModule),
+                typeof(EquipmentSlotModule),
+                typeof(LootDropModule),
+                typeof(TileSetModule),
+                typeof(UserInterfaceModule)
+            );
+        }
+
+        public void Init()
+        {
+        }
+
+        public void Shutdown()
+        {
+        }
+
+        public void ModObjectLoaded(Object obj)
+        {
+        }
+
+        public void Update()
+        {
+        }
+    }
+}
